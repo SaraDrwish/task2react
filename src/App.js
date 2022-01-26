7c4322898d30241dcb324c247533b075ce7b1667
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-01-26 22:03:52 +0300

    1th

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends component {
+   state= {
+     product1: [ {
+       title:"pro1" ,
+       id: "12",
+       price: 100 ,
+       model: "22222",
+     } ],
+     product2:[ 
+       {  title:"pro2" ,
+     id: "123",
+     price: 200 ,
+     model: "22222",
+    }
+    ],
+     product3:[
+      {
+        title:"pro3" ,
+        id: "1234",
+        price: 300 ,
+        model: "22222",
+      }
+     ]
+
+
+   };
+ }
 
 export default App;
